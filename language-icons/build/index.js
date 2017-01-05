856b4280521e5a364c6f09f61186df6c04994505
StepanMas <stepan.mas.work@gmail.com>
2017-01-05 17:30:26 +0400

    small update

--- language-icons/build/index.js
+++ language-icons/build/index.js
@@ -50,7 +50,7 @@
 
 	__webpack_require__(2);
 
-	console.log(111);
+	console.log('1');
 
 /***/ },
 /* 1 */
--- language-icons/index.js
+++ language-icons/index.js
@@ -1,2 +1,4 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import 'page/style.less';
+
+console.log('1');
